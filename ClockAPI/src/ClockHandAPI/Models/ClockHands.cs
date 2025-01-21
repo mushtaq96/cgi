a796822674838e69d5d2e6d183d32f60ebb09d60
mushtaq96 <mushtaq96smb@gmail.com>
2025-01-21 23:23:19 +0100

    feat: add model structure for task

---
+++ ClockAPI/src/ClockHandAPI/Models/ClockHands.cs
@@ -0,0 +1,8 @@
+namespace ClockHandAPI.Models
+{
+    // This model represents the core domain object, encapsulating the clock hand angles
+    public class ClockHands{
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+    }
+}
