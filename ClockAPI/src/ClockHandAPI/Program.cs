824dc1ef9496f2235bf4245ffbc9affac2d54ea0
mushtaq96 <mushtaq96smb@gmail.com>
2025-01-22 00:27:31 +0100

    update: entry file Program.cs

--- ClockAPI/src/ClockHandAPI/Program.cs
+++ ClockAPI/src/ClockHandAPI/Program.cs
@@ -1,6 +1,6 @@
 using ClockHandAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers(); // extension method that registers the services required for controllers
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
